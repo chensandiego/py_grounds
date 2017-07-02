9ca3a609c6409f3883ed092d281cfd2ab2c1ef82
chen-ming chen <chen@chen-mings-MacBook-Pro.local>
2017-07-02 12:29:58 +0800

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+# this repository includes different python codes 
---
+++ text_analyzer.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TextAnalysisTests(unittest.TestCase):
+    """tests for the analyze_text function"""
+    def test_function_runs(self):
+        analyze_text()
+
+
+
+
+def analyze_text():
+    pass
+
+if __name__=='__main__':
+    unittest.main()
